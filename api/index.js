05866daf54009e9ce7872e2130bf727c3f75fdd0
OgutuJoseph <onyango.joseph@yahoo.com>
2022-07-04 14:13:13 +0300

    Creating connection to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,32 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        // await mongoose.connect('mongodb://localhost:27017/hotel-booking')
+        await mongoose.connect(process.env.MONGO)
+        console.log('Connected to mongodb.')
+    } catch (error) {
+        // handleError(error)
+        throw error
+    }
+}
+
+mongoose.connection.on('connected', () => {
+    console.log('Database connected.');
+})
+mongoose.connection.on('disconnected', () => {
+    console.log('Database disconnected.');
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello first request!');
+})
 
 app.listen(8300, () => {
+    connect();
     console.log('Connected to the server.')
 })
