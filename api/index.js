c6f0b04dfb602fd000dde766692c44fd01efb768
OgutuJoseph <onyango.joseph@yahoo.com>
2022-07-05 11:18:31 +0300

    Error hadling uains express middleware

--- api/index.js
+++ api/index.js
@@ -28,19 +28,25 @@ mongoose.connection.on('disconnected', () => {
 })
 
 // middlewares
-
-app.use((req, res, next) => {
-    console.log('hi, im a middleware.')
-    next();
-})
-
 app.use(express.json());
-
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
+
+app.use((err, req, res, next) => {
+    // return res.status(500).json('An error occured! From Handler.');
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || 'Something went wrong! From Handler.'
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
+
 
 
 app.listen(8300, () => {
