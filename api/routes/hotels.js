50c4ce6998c6fef1df99d019a656bf3e4b2db044
OgutuJoseph <onyango.joseph@yahoo.com>
2022-07-05 13:23:35 +0300

    Authentication - regisering and logging in users - using node and mongo

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,65 +1,22 @@
 import express from 'express';
-import Hotel from '../models/Hotel.js';
-import { createError } from '../utils/error.js';
+import { createHotel, updateHotel, deleteHotel, getHotel, getHotels } from '../controllers/hotel.js';
 
 const router = express.Router();
 
-// create
-router.post('/', async (req, res) => {
+// create route
+router.post('/', createHotel);
 
-    const newHotel = Hotel(req.body)
-    
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
+// update route
+router.put('/:id', updateHotel);
 
-// update
-router.put('/:id', async (req, res) => {
+// delete route
+router.delete('/:id', deleteHotel);
 
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-        res.status(200).json(updatedHotel);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
+// get route
+router.get('/:id', getHotel)
 
-// delete
-router.delete('/:id', async (req, res) => {
-
-    try {
-        await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json('Hotel has been deleted.');
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
-
-// get
-router.get('/:id', async (req, res) => {
-
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
-
-// get all
-router.get('/', async (req, res, next) => {
-    
-    try {
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
+// get all route
+router.get('/', getHotels)
 
 // get all - test express error handler 1
 // router.get('/', async (req, res, next) => {
