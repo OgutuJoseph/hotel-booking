245c180808d51bcd72341a6bd7e6c1f740d99409
OgutuJoseph <onyango.joseph@yahoo.com>
2022-07-06 10:19:41 +0300

    Creating database relations

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import { createRoom, updateRoom, deleteRoom, getRoom, getRooms } from '../controllers/room.js';
+import { verifyUser, verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
+// create route
+router.post('/:hotelid', verifyAdmin, createRoom);
 
+// update route
+router.put('/:id', verifyAdmin, updateRoom);
+
+// delete route
+router.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
+
+// get route
+router.get('/:id', getRoom)
+
+// get all route
+router.get('/', getRooms)
 
 export default router;
